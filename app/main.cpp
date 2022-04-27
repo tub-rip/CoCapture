dea571bd200a976f88455bde5b882b8f7ce3b139
Friedhelm Hamann <f.hamann@posteo.net>
2022-04-27 16:06:13 +0200

    clean-up

--- app/main.cpp
+++ app/main.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 
 #include <pangolin/var/var.h>
-#include <pangolin/var/varextra.h>
 #include <pangolin/gl/gl.h>
-#include <pangolin/gl/gldraw.h>
 #include <pangolin/display/display.h>
 #include <pangolin/display/view.h>
 #include <pangolin/display/widgets.h>
 #include <pangolin/display/default_font.h>
-#include <pangolin/handler/handler.h>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
 
 
 int main() {
