aeca439df9fc93056f6c1204c65313cbfede1399
Friedhelm Hamann <f.hamann@posteo.net>
2022-04-28 07:57:30 +0200

    prophesee interface skeleton

---
+++ lib/prophesee_interface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by friedhelm on 28.04.22.
+//
+
+#include "prophesee_interface.h"
---
+++ lib/prophesee_interface.h
@@ -0,0 +1,16 @@
+//
+// Created by friedhelm on 28.04.22.
+//
+
+#ifndef RIP_COCAPTURE_GUI_PROPHESEE_INTERFACE_H
+#define RIP_COCAPTURE_GUI_PROPHESEE_INTERFACE_H
+
+
+class PropheseeInterface {
+public:
+
+private:
+};
+
+
+#endif //RIP_COCAPTURE_GUI_PROPHESEE_INTERFACE_H
