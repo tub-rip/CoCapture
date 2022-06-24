f3924e684a925b453ef329b0ef0293585d868986
Friedhelm Hamann <f.hamann@posteo.net>
2022-06-24 15:31:42 +0200

    added grab result check in basler callback

--- lib/basler_event_handler.cpp
+++ lib/basler_event_handler.cpp
@@ -10,10 +10,18 @@ namespace basler {
     void BaslerEventHandler::OnImageGrabbed(Pylon::CInstantCamera &camera,
                                             const Pylon::CGrabResultPtr &grab_result) {
         // TODO: include if grab succeeded
-        std::unique_lock<std::mutex> lock(m_);
-        cv::Mat frame(grab_result->GetHeight(), grab_result->GetWidth(),
-                      CV_8UC1, (uint8_t*)grab_result->GetBuffer());
-        cv::cvtColor(frame, img_, cv::COLOR_GRAY2RGB);
+        if (grab_result->GrabSucceeded()) {
+            std::unique_lock<std::mutex> lock(m_);
+            cv::Mat frame(grab_result->GetHeight(), grab_result->GetWidth(),
+                          CV_8UC1, (uint8_t*)grab_result->GetBuffer());
+            cv::cvtColor(frame, img_, cv::COLOR_GRAY2RGB);
+        }
+        else {
+            std::cout << "Error: " << std::hex << grab_result->GetErrorCode() <<
+            std::dec << " " << grab_result->GetErrorDescription() << std::endl;
+        }
+
+
     }
 
 } // basler
