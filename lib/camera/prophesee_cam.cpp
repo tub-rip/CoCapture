e851c33e7ca2140611016cf1783d1c8baf551197
friedhelm <f.hamann@posteo.net>
2022-12-02 11:29:07 +0100

    formatting

--- lib/camera/prophesee_cam.cpp
+++ lib/camera/prophesee_cam.cpp
@@ -3,100 +3,100 @@
 
 namespace camera {
 
-  void PropheseeCam::setup_camera() {
-    cam_ = Metavision::Camera::from_first_available();
-    cam_.erc_module().enable(false);
-    std::cout << "Opening Prophesee Camera: " << cam_.get_camera_configuration().serial_number << std::endl;
-    auto &geometry = cam_.geometry();
-    width_ = geometry.width();
-    height_ = geometry.height();
-    visualizer_.setup_display(width_, height_, false);
+    void PropheseeCam::setup_camera() {
+        cam_ = Metavision::Camera::from_first_available();
+        cam_.erc_module().enable(false);
+        std::cout << "Opening Prophesee Camera: " << cam_.get_camera_configuration().serial_number << std::endl;
+        auto &geometry = cam_.geometry();
+        width_ = geometry.width();
+        height_ = geometry.height();
+        visualizer_.setup_display(width_, height_, false);
 
-    cam_.add_runtime_error_callback([](const Metavision::CameraException &e) {
-        MV_LOG_ERROR() << e.what();
-        throw;
-      });
+        cam_.add_runtime_error_callback([](const Metavision::CameraException &e) {
+            MV_LOG_ERROR() << e.what();
+            throw;
+        });
 #ifdef USE_METAVISION_VIEWER
-    cd_frame_generator = new Metavision::CDFrameGenerator(width_, height_);
-    cd_frame_generator->set_display_accumulation_time_us(params_.ev_acc_t_ms);
-    auto id = cam_.cd().add_callback([this](const Metavision::EventCD *ev_begin,
-                                     const Metavision::EventCD *ev_end) {
-        cd_frame_generator->add_events(ev_begin, ev_end);
-      });
-    cd_frame_generator->start(params_.fps, [this](const Metavision::timestamp &ts, const cv::Mat &frame) {
-        frame.copyTo(cd_frame_);
-      });
+        cd_frame_generator = new Metavision::CDFrameGenerator(width_, height_);
+        cd_frame_generator->set_display_accumulation_time_us(params_.ev_acc_t_ms);
+        auto id = cam_.cd().add_callback([this](const Metavision::EventCD *ev_begin,
+                                         const Metavision::EventCD *ev_end) {
+            cd_frame_generator->add_events(ev_begin, ev_end);
+          });
+        cd_frame_generator->start(params_.fps, [this](const Metavision::timestamp &ts, const cv::Mat &frame) {
+            frame.copyTo(cd_frame_);
+          });
 #else
-    cam_.cd().add_callback([this](const Metavision::EventCD *begin,
-                           const Metavision::EventCD *end) {
-        visualizer_.process_events(begin, end);
-      });
+        cam_.cd().add_callback([this](const Metavision::EventCD *begin,
+                                      const Metavision::EventCD *end) {
+            visualizer_.process_events(begin, end);
+        });
 
-    cam_.cd().add_callback([this](const Metavision::EventCD *begin,
-                           const Metavision::EventCD *end) {
-        visualizer_.estimate_snr(begin, end);
-      });
+        cam_.cd().add_callback([this](const Metavision::EventCD *begin,
+                                      const Metavision::EventCD *end) {
+            visualizer_.estimate_snr(begin, end);
+        });
 #endif
 
-    if (params_.mode == "master") {
-        this->set_mode_master();
-      } else if (params_.mode == "slave") {
-        this->set_mode_slave();
-      }
+        if (params_.mode == "master") {
+            this->set_mode_master();
+        } else if (params_.mode == "slave") {
+            this->set_mode_slave();
+        }
 
-    if (params_.set_rois) {
-        utils::set_roi(cam_);
-      }
+        if (params_.set_rois) {
+            utils::set_roi(cam_);
+        }
 
-    if (params_.record_from_startup) {
-        std::stringstream ss;
-        ss << "./out_" << params_.id << ".raw";
-        cam_.start_recording(ss.str());
-      }
-    std::cout<<"Before starting streaming \n";
-    cam_.start();
-  }
+        if (params_.record_from_startup) {
+            std::stringstream ss;
+            ss << "./out_" << params_.id << ".raw";
+            cam_.start_recording(ss.str());
+        }
+        std::cout << "Before starting streaming \n";
+        cam_.start();
+    }
 
-  void PropheseeCam::get_display_frame(cv::Mat &display) {
+    void PropheseeCam::get_display_frame(cv::Mat &display) {
 #ifdef USE_METAVISION_VIEWER
-    if (!cd_frame_.empty()) {
-        cv::flip(cd_frame_, display, 0);
-      }
+        if (!cd_frame_.empty()) {
+            cv::flip(cd_frame_, display, 0);
+          }
 #else
-    visualizer_.get_display_frame(display);
+        visualizer_.get_display_frame(display);
 #endif
-  }
+    }
 
-  void PropheseeCam::get_display_frame(cv::Mat &display, const cv::Mat &canvas) {
-    visualizer_.get_display_frame(display, canvas);
-  }
+    void PropheseeCam::get_display_frame(cv::Mat &display, const cv::Mat &canvas) {
+        visualizer_.get_display_frame(display, canvas);
+    }
 
-  void PropheseeCam::start_camera() { cam_.start(); }
+    void PropheseeCam::start_camera() { cam_.start(); }
 
-  void PropheseeCam::stop_camera() { cam_.stop(); }
+    void PropheseeCam::stop_camera() { cam_.stop(); }
 
-  void PropheseeCam::set_mode_master() {
-    Metavision::Device &device = cam_.get_device();
-    auto *i_device_control = device.get_facility<Metavision::I_DeviceControl>();
-    if (i_device_control->set_mode_master()) {
-        std::cout << "Set mode Master successful."
-                     "Remember to start the slave first." << std::endl;
-      } else {
-        std::cerr << "Could not set Master mode. Master/slave might"
-                     "not be supported by your camera" << std::endl;
-      }
-  }
+    void PropheseeCam::set_mode_master() {
+        Metavision::Device &device = cam_.get_device();
+        auto *i_device_control = device.get_facility<Metavision::I_DeviceControl>();
+        if (i_device_control->set_mode_master()) {
+            std::cout << "Set mode Master successful."
+                         "Remember to start the slave first." << std::endl;
+        } else {
+            std::cerr << "Could not set Master mode. Master/slave might"
+                         "not be supported by your camera" << std::endl;
+        }
+    }
 
-  void PropheseeCam::set_mode_slave() {
-    Metavision::Device &device = cam_.get_device();
-    auto *i_device_control = device.get_facility<Metavision::I_DeviceControl>();
-    if (i_device_control->set_mode_slave()) {
-        std::cout << "Set mode Slave successful." << std::endl;
-      } else {
-        std::cerr << "Could not set Slave mode. Master/slave might"
-                     "not be supported by your camera" << std::endl;
-      }
-  }
+    void PropheseeCam::set_mode_slave() {
+        Metavision::Device &device = cam_.get_device();
+        auto *i_device_control = device.get_facility<Metavision::I_DeviceControl>();
+        if (i_device_control->set_mode_slave()) {
+            std::cout << "Set mode Slave successful." << std::endl;
+        } else {
+            std::cerr << "Could not set Slave mode. Master/slave might"
+                         "not be supported by your camera" << std::endl;
+        }
+    }
 
 
 } // camera
--- lib/camera/prophesee_cam.h
+++ lib/camera/prophesee_cam.h
@@ -16,31 +16,35 @@
 
 namespace camera {
 
-  class PropheseeCam : public Base {
-  public:
-    explicit PropheseeCam(PropheseeParams &params) : params_(params) {};
-    explicit PropheseeCam(PropheseeParams params) : params_(params) {};
+    class PropheseeCam : public Base {
+    public:
+        explicit PropheseeCam(PropheseeParams &params) : params_(params) {};
 
-    void get_display_frame(cv::Mat &display) override;
+        explicit PropheseeCam(PropheseeParams params) : params_(params) {};
 
-    void get_display_frame(cv::Mat &display, const cv::Mat &canvas);
+        void get_display_frame(cv::Mat &display) override;
 
-    void setup_camera() override;
+        void get_display_frame(cv::Mat &display, const cv::Mat &canvas);
 
-    void start_camera();
-    void stop_camera();
-    void set_mode_master();
-    void set_mode_slave();
+        void setup_camera() override;
 
-  private:
-    Metavision::Camera cam_;
-    EventVisualizer visualizer_;
-    PropheseeParams params_;
-    cv::Mat cd_frame_;
+        void start_camera();
+
+        void stop_camera();
+
+        void set_mode_master();
+
+        void set_mode_slave();
+
+    private:
+        Metavision::Camera cam_;
+        EventVisualizer visualizer_;
+        PropheseeParams params_;
+        cv::Mat cd_frame_;
 #ifdef USE_METAVISION_VIEWER
-    Metavision::CDFrameGenerator *cd_frame_generator;
+        Metavision::CDFrameGenerator *cd_frame_generator;
 #endif
-  };
+    };
 
 
 } // camera
